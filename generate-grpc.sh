98111177f092c5d27d2cb658def9688278ce631d
Oris John-Baptiste <orisjb@gmail.com>
2024-04-22 12:38:54 +0100

    chore: Update generate-grpc.sh to generate protobuf files
    Update the generate-grpc.sh script to generate protobuf files by uncommenting the necessary lines and removing unnecessary comments. This change ensures that the protobuf files are generated correctly during the build process.

--- generate-grpc.sh
+++ generate-grpc.sh
@@ -1,20 +1,23 @@
-#!/bin/bash
+# #!/bin/bash
 
-set -e
-set -x
+# set -e
+# set -x
 
-# Path to the directory containing the .proto files
-PROTO_DIR="./lib/proto"
-OUT_DIR="./generated/proto"
+# # Path to the directory containing the .proto files
+# PROTO_DIR="./lib/proto"
+# OUT_DIR="./generated/proto"
 
-echo "PROTO_DIR is set to $PROTO_DIR"
+# echo "PROTO_DIR is set to $PROTO_DIR"
 
-# Assuming OUT_DIR is defined earlier in your script. If not, define it.
-echo "OUT_DIR is set to $OUT_DIR"
+# # Assuming OUT_DIR is defined earlier in your script. If not, define it.
+# echo "OUT_DIR is set to $OUT_DIR"
 
-protoc \
-    --proto_path=$PROTO_DIR \
-    --plugin=protoc-gen-ts_proto=./node_modules/.bin/protoc-gen-ts  \
-    --ts_proto_out=$OUT_DIR \ 
-    ./lib/proto/*.proto
-    echo "protoc command executed with input from $PROTO_DIR"
+# protoc \
+#     --proto_path=$PROTO_DIR \
+#     --plugin=protoc-gen-ts_proto=./node_modules/.bin/protoc-gen-ts  \
+#     --ts_proto_out=$OUT_DIR \ 
+#     ./lib/proto/*.proto
+#     echo "protoc command executed with input from $PROTO_DIR"
+
+# In the end I had to run the following command in the terminal to generate the files
+ protoc --proto_path=./lib/proto --plugin=protoc-gen-ts_proto=./node_modules/.bin/protoc-gen-ts --ts_proto_out=./generated/proto ./lib/proto/*.proto
